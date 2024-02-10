8abced931c481c21e860d5add5c60d0db4042b2f
Helda <heldaapga@gmail.com>
2024-02-10 20:11:31 +0700

    tambah file tugasjs  nomor 1

---
+++ tugas js pekan 3 nomor 1.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({signint: true});
+const number = prompt('Masukkan angka: ');
+
+const result = Math.sqrt(number);
+if (!isNaN(number) && number >= 0) { //bilangan genap dimulai dari angka 2 dst
+    if (number % 2 === 0) {
+      console.log("Ini bilangan genap"); 
+    } else { //jika input angka ganjil
+      console.log("Tidak bisa input bilangan ganjil");
+    }
+  } else { //jika input angka kurang dari 0
+    console.log("Tidak bisa input bilangan negatif");
+  }
