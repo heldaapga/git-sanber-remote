80447503d8c0c6f47be35828436f12ea18e85a13
Helda <heldaapga@gmail.com>
2024-02-19 00:00:03 +0700

    update tugas js

--- javascript/tugas-js-1.js
+++ javascript/tugas-js-1.js
@@ -5,6 +5,8 @@ const result = Math.sqrt(number);
 if (!isNaN(number) && number >= 0) { //bilangan genap dimulai dari angka 2 dst
     if (number % 2 === 0) {
       console.log("Ini bilangan genap"); 
+      console.log(`Akar kuadrat dari ${number} adalah ${result}`);
+
     } else { //jika input angka ganjil
       console.log("Tidak bisa input bilangan ganjil");
     }
